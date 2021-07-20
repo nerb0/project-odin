8067af5ecc95043809c136fcd4f0538a7f7a4712
Bren Steven Javier <bso.javier0125@gmail.com>
2021-07-20 10:42:20 +0800

    Added substring exercise

---
+++ ruby/substring/substring.rb
@@ -0,0 +1,10 @@
+def substrings(words, arr)
+    res = {};
+    arr.each {|string| 
+        count = words.downcase.scan(string).length
+        res[string] = count unless count < 1
+    }
+    p res
+end
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("Howdy partner, sit down! How's it going?", dictionary)
