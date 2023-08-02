b905fb7ab5a0b6210d49e54f08739629693a55d3
Bren Javier <12233168-nerb-visage@users.noreply.gitlab.com>
2023-08-02 22:11:18 +0800

    fix: renamed weather api key from .env - API_KEY to WEATHER_API_KEY

--- js/weather-api/src/controllers/Forecast.ts
+++ js/weather-api/src/controllers/Forecast.ts
@@ -1,11 +1,11 @@
 export const WEATHER_API_URL = 'http://api.weatherapi.com/v1';
 
 export async function getForecastFromCity(city: string): Promise<ForecastList>  {
-	const result = await fetch(`${WEATHER_API_URL}/forecast.json?key=${process.env.API_KEY}&q=${city}&days=8`);
+	const result = await fetch(`${WEATHER_API_URL}/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${city}&days=8`);
 	return result.json();
 }
 
 export async function getForecastFromUserLocation(): Promise<ForecastList> {
-	const result = await fetch(`${WEATHER_API_URL}/forecast.json?key=${process.env.API_KEY}&q=auto:ip&days=8`);
+	const result = await fetch(`${WEATHER_API_URL}/forecast.json?key=${process.env.WEATHER_API_KEY}&q=auto:ip&days=8`);
 	return result.json();
 }
