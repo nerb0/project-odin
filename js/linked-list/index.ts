9679d4abeaaebe7a02639f64f928fd8ebc0e0df6
Bren Javier <12233168-nerb-visage@users.noreply.gitlab.com>
2023-08-03 15:24:14 +0800

    feat: test taken from luaroxy

---
+++ js/linked-list/index.ts
@@ -0,0 +1,25 @@
+import { LinkedList } from "./LinkedList";
+
+const linkedList = new LinkedList();
+
+linkedList.prepend("test1");
+linkedList.append("test2");
+linkedList.append("test3");
+console.log(linkedList.toString()); // (test1) -> (test2) -> (test3) -> null
+console.log(linkedList.size()); // 3
+console.log(linkedList.head); // return head Node
+console.log(linkedList.tail); // Node { value: 'test3', nextNode: null }
+console.log(linkedList.at(2)); // Node { value: 'test3', nextNode: null }
+console.log(linkedList.at(4)); // There is no item for this index
+console.log(linkedList.pop()); // Node { value: 'test3', nextNode: null }
+console.log(linkedList.toString()); // null
+console.log(linkedList.contains("test1")); // true
+console.log(linkedList.find("test2")); // 1
+linkedList.prepend("test3");
+console.log(linkedList.toString()); // (test3) -> (test1) -> (test2) -> null
+linkedList.insertAt("test4",2);
+console.log(linkedList.toString()); // (test3) -> (test1) -> (test4) -> (test2) -> null
+linkedList.insertAt("test5",8); // Index out of bounds
+console.log(linkedList.toString()); // (test3) -> (test1) -> (test4) -> (test2) -> null
+linkedList.removeAt(2);
+console.log(linkedList.toString()); // (test3) -> (test1) -> (test4) -> null
