476c0db96e6326c1214b8c1541965f4e31d5b169
Bren Javier <12233168-nerb-visage@users.noreply.gitlab.com>
2023-08-19 23:05:53 +0800

    fix: unnessecary files

--- react/memory-card/src/App.tsx
+++ react/memory-card/src/App.tsx
@@ -5,7 +5,7 @@ import { shuffle } from "./util";
 
 function App() {
 	return (
-		<div className="bg-gradient-to-tl from-gray-600 to-gray-700 p-2 h-screen w-screen">
+		<div className="bg-gradient-to-tl from-gray-600 to-gray-700 p-2 h-full w-full">
 			<MemoryGame cards={shuffle(GameCards).splice(0, 12)} />
 		</div>
 	);
