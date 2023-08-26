f70d65cded07e11a89bc63ef819a4cbe186174ad
Bren Javier <12233168-nerb-visage@users.noreply.gitlab.com>
2023-08-26 15:26:17 +0800

    fix: mongodb connection and env variables for client

--- nodejs/mini-message-board/client/src/utils/index.ts
+++ nodejs/mini-message-board/client/src/utils/index.ts
@@ -1,4 +1,4 @@
-const getRoute = (route: string) => `${import.meta.env.API_URL}/${route}`;
+const getRoute = (route: string) => `${import.meta.env.VITE_API_URL}/${route}`;
 
 export async function corsFetch(route: string, options?: RequestInit) {
 	return await fetch(getRoute(route), { ...options, mode: "cors" });
--- nodejs/mini-message-board/server/src/index.ts
+++ nodejs/mini-message-board/server/src/index.ts
@@ -1,7 +1,13 @@
 import cors from "cors";
 import express from "express";
 import router from "./routes";
+import client from "./db";
 
+try {
+	await client.connect();
+} catch (err) {
+	console.log("Error connecting to the database", err);
+}
 const { PORT = 3000 } = process.env;
 const server = express();
 
