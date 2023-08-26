79f0452b05c7681ae5261bfd63ee41557f61cdf2
Bren Javier <12233168-nerb-visage@users.noreply.gitlab.com>
2023-08-26 15:16:16 +0800

    fix: put api endpoint url to .env

--- nodejs/mini-message-board/client/src/utils/index.ts
+++ nodejs/mini-message-board/client/src/utils/index.ts
@@ -1,6 +1,4 @@
-const API_URL = "http://localhost:3000";
-
-const getRoute = (route: string) => `${API_URL}/${route}`;
+const getRoute = (route: string) => `${import.meta.env.API_URL}/${route}`;
 
 export async function corsFetch(route: string, options?: RequestInit) {
 	return await fetch(getRoute(route), { ...options, mode: "cors" });
