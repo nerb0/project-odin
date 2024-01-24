58bafae7a66bb974953d79b3ee4f291365991370
Bren Javier <119868598+bsojj@users.noreply.github.com>
2024-01-24 19:20:51 +0800

    renamed nodejs to backend since i will be trying golang

--- nodejs/members-only/src/constants.js
+++
@@ -1,1 +0,0 @@
-export const { NODE_ENV = "development" } = process.env;
--- nodejs/members-only/types.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-	declare module "hono" {
-		interface ContextRenderer {
-			(content: string | Promise<string>, props: { title: string }): Response;
-		}
-	}
-}
